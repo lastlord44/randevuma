18a5bc20e798e5a552576307cd35a0a2a1545b73
polfule <polfules@gmail.com>
2025-10-23 21:28:36 +0300

    feat: Turso cloud SQLite integration

--- randevuma v.1.0/lib/db.ts
+++ randevuma v.1.0/lib/db.ts
@@ -1,11 +1,29 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+// Turso (libSQL) ise adapter kullan
+let prisma: PrismaClient;
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const isTurso = !!process.env.TURSO_DATABASE_URL;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (isTurso) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { createClient } = require("@libsql/client");
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { PrismaLibSQL } = require("@prisma/adapter-libsql");
 
+  const libsql = createClient({
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
+  });
+  const adapter = new PrismaLibSQL(libsql);
+  prisma = new PrismaClient({ adapter });
+} else {
+  prisma = new PrismaClient();
+}
 
+// Hot-reload safe (Next dev)
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+export const db = globalForPrisma.prisma || prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
+export { db as prisma };
