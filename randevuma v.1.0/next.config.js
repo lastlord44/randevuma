eab59a817b7fc0af711046614f4b3a8ed43a099c
polfule <polfules@gmail.com>
2025-10-23 21:35:13 +0300

    fix: mark libsql as external in webpack config

--- randevuma v.1.0/next.config.js
+++ randevuma v.1.0/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
+  experimental: {
+    serverComponentsExternalPackages: ['@libsql/client', '@prisma/adapter-libsql'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('@libsql/client', '@prisma/adapter-libsql');
+    }
     config.module.rules.push({
       test: /\/(README\.md|LICENSE)$/i,
       type: 'asset/source',
